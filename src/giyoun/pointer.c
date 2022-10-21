48cdf1da49f4cefc33719c008eb753e370f3d389
Ubuntu <ubuntu@ip-172-31-6-242.ap-northeast-2.compute.internal>
2022-10-21 18:43:48 +0000

    test

---
+++ src/giyoun/pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int* ptr_a, int* ptr_b){
+    int temp = *ptr_a;
+    *ptr_a = *ptr_b;
+    *ptr_b = temp;
+}
+int main(){
+    int a=10;
+    int b=20;
+
+    printf("a : %d, b= %d\n",a,b);
+    swap(&a,&b);
+    printf("a : %d, b = %d\n",a,b);
+    return 0;
+}
