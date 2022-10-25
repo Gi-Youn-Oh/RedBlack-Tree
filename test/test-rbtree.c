bbb4436fd2e7368415b7125577aeba86b4f2ac7c
Gi-Youn-Oh <109953972+Gi-Youn-Oh@users.noreply.github.com>
2022-10-26 01:52:58 +0900

    tst

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -1,8 +1,8 @@
-#include <assert.h>
+#include <assert.h> // check debug
 #include <rbtree.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // 동적 메모리 할당 , calloc or malloc
 
 // new_rbtree should return rbtree struct with null root node
 void test_init(void) {
